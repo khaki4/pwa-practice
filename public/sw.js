106ce6f790b447d943580f99c5045ce6ae14d324
pi3rrot <4dakto@gmail.com>
2018-11-07 19:05:06 +0900

    Cache & Routing

--- public/sw.js
+++ public/sw.js
@@ -77,7 +77,9 @@ self.addEventListener('fetch', function(event) {
               .catch(function(err) {
                 return caches.open(CACHE_STATIC_NAME)
                   .then(cache => {
-                    return cache.match('./offline.html');
+                    if (event.request.url.indexOf('/help')) {
+                      return cache.match('./offline.html');
+                    }
                   })
               });
           }
