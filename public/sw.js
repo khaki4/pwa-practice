19bf6968ba2d88ff9e8717dfb0053445ed70a9ad
pi3rrot <4dakto@gmail.com>
2018-11-07 18:49:00 +0900

    added supporting offline

--- public/sw.js
+++ public/sw.js
@@ -46,16 +46,44 @@ self.addEventListener('activate', function(event) {
 
 // Cache then Network
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(CACHE_DYNAMIC_NAME)
-      .then((cache) => {
-        return fetch(event.request)
-          .then((res) => {
-            cache.put(event.request, res.clone());
-            return res;
-          });
-      })
-  );
+  const url = 'https://httpbin.org/get';
+
+  if (event.request.url.indexOf(url) > -1) {
+    event.respondWith(
+      caches.open(CACHE_DYNAMIC_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((res) => {
+              cache.put(event.request, res.clone());
+              return res;
+            });
+        })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          } else {
+            return fetch(event.request)
+              .then(function(res) {
+                return caches.open(CACHE_DYNAMIC_NAME)
+                  .then(function(cache) {
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                  })
+              })
+              .catch(function(err) {
+                return caches.open(CACHE_STATIC_NAME)
+                  .then(cache => {
+                    return cache.match('./offline.html');
+                  })
+              });
+          }
+        })
+    );
+  }
 });
 
 // Cache then Network
